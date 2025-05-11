9b8220c2ea7957fa4a25edb406958c4aac1e7df7
Ofir Semel <semelofir@gmail.com>
2025-05-11 22:37:18 +0300

    added about page - half way thro

--- services/global.js
+++ services/global.js
@@ -1,26 +1,22 @@
-
-const style = document.createElement('style');
-
-style.textContent = 
-`
-  nav ul li a::after {
-    content: '';
-    position: absolute;
-    width: 0;
-    height: 2px;
-    background: #ff66c4;
-    left: 0;
-    bottom: -5px;
-    transition: width .3s;
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('open');
   }
+  
 
-  nav ul li a:hover::after {
-    width: 100%;
-  }
+ 
 
-  nav ul li a {
-    position: relative;
-  }
-`;
-
-document.head.appendChild(style);
+window.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-links a');
+    const currentPage = window.location.pathname.split('/').pop();
+  
+    navLinks.forEach(link => {
+      const linkPage = link.getAttribute('href');
+      if (linkPage === currentPage) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  });
+  
